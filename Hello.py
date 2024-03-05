b926d0c5277c855aafdde43b8916f4151c2220dd
Fernando Pérez-García <fperezgarcia@microsoft.com>
2024-03-05 13:24:13 -0800

    Add basic code

--- Hello.py
+++ Hello.py
@@ -1,16 +1,4 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+import datetime
 
 import streamlit as st
 from streamlit.logger import get_logger
@@ -18,34 +6,102 @@ from streamlit.logger import get_logger
 LOGGER = get_logger(__name__)
 
 
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
-
-    st.write("# Welcome to Streamlit! 👋")
-
-    st.sidebar.success("Select a demo above.")
-
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)
 
 
-if __name__ == "__main__":
-    run()
+class RSU:
+    def __init__(
+        self,
+        name: str,
+        date: datetime.date,
+        total: float,
+        cliff_years: int = 0,
+    ):
+        self.name = name
+        self.date = date
+        self.total = total
+        self.cliff_days = 365 * cliff_years
+        self.amounts = self.compute_amounts()
+
+    @staticmethod
+    def get_next_date(date: datetime.date):
+        next_month = date.month + 3
+        if next_month > 12:
+            return datetime.date(date.year + 1, next_month - 12, date.day)
+        else:
+            return datetime.date(date.year, next_month, date.day)
+
+    def compute_amounts(self, end_date: datetime.date = datetime.date(year=2032, month=12, day=31)) -> dict[datetime.date, float]:
+        amounts = {}
+        vested = 0
+        date = self.date
+        next_date = RSU.get_next_date(date)
+        acc_percentage = 0.0625
+        while True:
+            if date == next_date:
+                if date < self.date + datetime.timedelta(days=self.cliff_days):
+                    acc_percentage += 0.0625
+                elif vested < self.total:
+                    vested += self.total * acc_percentage
+                    acc_percentage = 0.0625
+                next_date = RSU.get_next_date(next_date)
+            amounts[date] = vested
+            date += datetime.timedelta(days=1)
+            if date == end_date:
+                break
+        return amounts
+
+
+def main():
+    if 'rsus' not in st.session_state:
+        st.session_state.rsus = {}
+    st.markdown("# RSUs")
+    st.sidebar.header("RSUs")
+    st.write('Test')
+
+    name_col, total_col, start_col, cliff_col, add_col = st.columns(5)
+
+    name = name_col.text_input('Award type', value='On-Hire')
+    total = total_col.number_input('Total $', value=20_000)
+    start_date = start_col.date_input('Start date', value=datetime.date(year=2022, month=3, day=15))
+    cliff_years = cliff_col.number_input('Cliff years', value=1)
+    st.session_state.rsus[name] = RSU(name, start_date, total, cliff_years)
+    add_col.button('Add', on_click=update)
+
+
+def update():
+    import matplotlib.pyplot as plt
+
+    fig, axis = plt.subplots(figsize=(10, 5))
+
+    def plot_amounts(amounts: dict[datetime.date, float], name: str, color=None):
+        dates = list(amounts.keys())
+        values = list(amounts.values())
+        axis.plot(dates, values, label=name, color=color)
+
+    for name, rsu in st.session_state.rsus.items():
+        plot_amounts(rsu.amounts, name)
+
+    amounts = [rsu.amounts for rsu in st.session_state.rsus.values()]
+    all_amounts = add_dicts(*amounts)
+    plot_amounts(all_amounts, 'Total', color='black')
+    axis.legend()
+    axis.grid()
+    st.pyplot(fig)
+
+
+def add_dicts(*dicts) -> dict:
+    all_keys = set()
+    for d in dicts:
+        all_keys.update(d.keys())
+    result = {}
+    for k in sorted(all_keys):
+        result[k] = sum(d.get(k, 0) for d in dicts)
+    return result
+
+
+if __name__ == '__main__':
+    main()
--- pages/4_RSUs.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-
-st.set_page_config(page_title="RSUs", page_icon="📈")
-st.markdown("# RSUs")
-st.sidebar.header("RSUs")
-st.write('Test')
